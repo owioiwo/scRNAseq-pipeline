fb5eac5fa931a6aad1175b3f428715bcc43cfa66
owioiwo <103429402+owioiwo@users.noreply.github.com>
2022-08-02 16:02:47 +0000

    Update 5_gene_cell_analysis.R
    fix bug: plot error if one gene is marker gene of more than one cluster

--- scripts/5_gene_cell_analysis.R
+++ scripts/5_gene_cell_analysis.R
@@ -28,7 +28,7 @@ cat("data loaded,check summary and two plots.. \n")
 # 2 find markers
 seurat.markers = FindAllMarkers(seurat.object)
 repeat{
-  cat("show how many top expressed genes,default is 5 : \n")
+  cat("show how many top differential expressed genes in eatch cluster,default is 5: \n")
   top.number = readline(prompt = "input a top exp number:")
   if(top.number ==""){
     top.number = 5
@@ -44,29 +44,30 @@ repeat{
 }
 top.markers = seurat.markers %>% filter(p_val_adj < 0.05) %>% group_by(cluster) %>% slice_max(n = top.number, order_by = avg_log2FC)
 top.markers = top.markers[,"gene"] %>% unique()
-cat("plot top",top.number,"makers in each cluster,check : \n",top.markers$gene)
+cat("plot top",top.number,"makers in each cluster,check : \n",top.markers)
 print(DotPlot(seurat.object,features = top.markers) + RotatedAxis())
 write.table(seurat.markers,paste0("markers_",inputdata_list[reso,],".txt"),sep = "\t",quote = F,row.names = F)
-cat("find markers done,all markers save as 'markers_data_name.txt'.. \n")
+cat("find markers done, save all markers as file: ", paste0("markers_",inputdata_list[reso,],".txt"),“ \n")
 # 3 plot marker genes and annotate cells
 repeat{
-  cat("Next we can do: \n 1 = plot gene expression by multi functions \n 2 = annotate cells by mannual \n 3 = annotate cell by R packages such as singleR \n 4 = end anaylsis \n")
+  cat("Next we can do: \n 1 = plot gene expression by multi functions \n 2 = annotate cells by mannual \n 3 = annotate cells by R package: SingleR \n 4 = end anaylsis \n")
   repeat{
     qc_choice = readline("1 or 2 or 3 or 4 : ")
     if(qc_choice %in% c("1","2","3","4")){break}
   }
   if(qc_choice == 1){
-    gene.plots(seurat.object,top.markers$gene)
+    gene.plots(seurat.object,top.markers)
   } else if(qc_choice == 2 | qc_choice == 3){
     seurat.object = cell.annotation(seurat.object,qc_choice)
   } else {
     UMAPPlot(seurat.object) -> plot1
     UMAPPlot(seurat.object, group.by = "sample") -> plot2
     print(plot1 + plot2)
-    cat("gene de analysis and cell annotation done,check plots.")
+    cat("gene de analysis and cell annotation done,check plots..\n")
     break
   } 
 }
 ##
+cat("save result as file: ",inputdata_list[reso,],“ \n")
 saveRDS(object = seurat.object,inputdata_list[reso,])
 # end
