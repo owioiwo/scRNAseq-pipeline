8dd1d45e7c91e839c41612982101ae6e4bab6333
owioiwo <13616543212@139.com>
2022-11-01 12:38:37 +0800

    fix a bug
    unlist top.markers

--- scripts/5_gene_cell_analysis.R
+++ scripts/5_gene_cell_analysis.R
@@ -43,11 +43,11 @@ repeat{
   }
 }
 top.markers = seurat.markers %>% filter(p_val_adj < 0.05) %>% group_by(cluster) %>% slice_max(n = top.number, order_by = avg_log2FC)
-top.markers = top.markers[,"gene"] %>% unique()
-cat("plot top",top.number,"makers in each cluster,check : \n",top.markers)
+top.markers = top.markers[,"gene"] %>% unique() %>% unlist()
+cat("plot top",top.number,"makers in each cluster,check : \n",top.markers,"\n")
 print(DotPlot(seurat.object,features = top.markers) + RotatedAxis())
 write.table(seurat.markers,paste0("markers_",inputdata_list[reso,],".txt"),sep = "\t",quote = F,row.names = F)
-cat("find markers done, save all markers as file: ", paste0("markers_",inputdata_list[reso,],".txt"),“ \n")
+cat("find markers done, save all markers as file: ", paste0("markers_",inputdata_list[reso,],".txt")," \n")
 # 3 plot marker genes and annotate cells
 repeat{
   cat("Next we can do: \n 1 = plot gene expression by multi functions \n 2 = annotate cells by mannual \n 3 = annotate cells by R package: SingleR \n 4 = end anaylsis \n")
@@ -68,6 +68,6 @@ repeat{
   } 
 }
 ##
-cat("save result as file: ",inputdata_list[reso,],“ \n")
+cat("save result as file: ",inputdata_list[reso,]," \n")
 saveRDS(object = seurat.object,inputdata_list[reso,])
 # end
